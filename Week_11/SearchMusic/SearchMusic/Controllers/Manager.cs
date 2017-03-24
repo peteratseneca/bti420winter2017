c10db652dc958db82a27c31becc26cdc5e3b349e
Peter McIntyre <peter.mcintyre@senecacollege.ca>
2017-03-24 10:33:37 -0400

    JavaScript and Ajax in our ASP.NET MVC web apps

---
+++ Week_11/SearchMusic/SearchMusic/Controllers/Manager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using SearchMusic.Models;
+
+namespace SearchMusic.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper components
+        MapperConfiguration config;
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add constructor code here
+
+            // Configure the AutoMapper components
+            config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+                // Object mapper definitions
+
+                cfg.CreateMap<Models.Track, Controllers.TrackBase>();
+
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        public IEnumerable<TrackBase> TrackGetAll()
+        {
+            return mapper.Map<IEnumerable<TrackBase>>(ds.Tracks.OrderBy(t => t.Name));
+        }
+
+        // Attention - 1 - Manager method to fetch tracks that match a search string
+        public IEnumerable<TrackBase> TrackGetAllByText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return null;
+            }
+
+            // Fetch the tracks that contain the search text
+            var c = ds.Tracks
+                .Include("Album.Artist")
+                .Include("MediaType")
+                .Include("Genre")
+                .Where(t => t.Name.ToLower().Contains(text.Trim().ToLower()));
+
+            return mapper.Map<IEnumerable<TrackBase>>(c.OrderBy(t => t.Name));
+        }
+
+    }
+}
