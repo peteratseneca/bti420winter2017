d734fa6a8219ccbabb6b14cc74b590985b0c9156
Peter McIntyre <peter.mcintyre@senecacollege.ca>
2017-03-31 08:45:19 -0400

    Data loading from CSV, XLSX, web service, etc.

---
+++ Week_12/WebServiceExample/WebServiceExample/Controllers/Manager.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using WebServiceExample.Models;
+using System.Security.Claims;
+
+namespace WebServiceExample.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper components
+        MapperConfiguration config;
+        public IMapper mapper;
+
+        // Request user property...
+
+        // Backing field for the property
+        private RequestUser _user;
+
+        // Getter only, no setter
+        public RequestUser User
+        {
+            get
+            {
+                // On first use, it will be null, so set its value
+                if (_user == null)
+                {
+                    _user = new RequestUser(HttpContext.Current.User as ClaimsPrincipal);
+                }
+                return _user;
+            }
+        }
+
+        public Manager()
+        {
+            // If necessary, add constructor code here
+
+            // Configure the AutoMapper components
+            config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+                // Object mapper definitions
+
+                cfg.CreateMap<Models.RegisterViewModel, Models.RegisterViewModelForm>();
+
+                cfg.CreateMap<Models.Artist, Controllers.ArtistBase>();
+                cfg.CreateMap<Models.Artist, Controllers.ArtistWithAlbums>();
+
+                cfg.CreateMap<Models.Album, Controllers.AlbumBase>();
+
+                cfg.CreateMap<Models.Employee, Controllers.EmployeeBase>();
+
+                // Change property names when mapping
+                cfg.CreateMap<Models.Employee, Controllers.EmployeeWithDetails>()
+                    .ForMember(dest => dest.ReportsTo, opt => opt.MapFrom(src => src.Employee2))
+                    .ForMember(dest => dest.DirectReports, opt => opt.MapFrom(src => src.Employee1));
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        // ############################################################
+        // Artist
+
+        // Attention - 3 - Normal "Manager" methods to get-all and get-one
+
+        public IEnumerable<ArtistBase> ArtistGetAll()
+        {
+            return mapper.Map<IEnumerable<ArtistBase>>(ds.Artists.OrderBy(a => a.Name));
+        }
+
+        public IEnumerable<ArtistWithAlbums> ArtistGetAllWithAlbums()
+        {
+            var c = ds.Artists.Include("Albums");
+
+            return mapper.Map<IEnumerable<ArtistWithAlbums>>(c.OrderBy(a => a.Name));
+        }
+
+        public ArtistBase ArtistGetById(int id)
+        {
+            var o = ds.Artists.Find(id);
+
+            return (o == null) ? null : mapper.Map<ArtistBase>(o);
+        }
+
+        public ArtistWithAlbums ArtistGetByIdWithAlbums(int id)
+        {
+            var o = ds.Artists.Include("Albums").SingleOrDefault(a => a.ArtistId == id);
+
+            return (o == null) ? null : mapper.Map<ArtistWithAlbums>(o);
+        }
+
+        // ############################################################
+        // Employee
+
+        public IEnumerable<EmployeeBase> EmployeeGetAll()
+        {
+            return mapper.Map<IEnumerable<EmployeeBase>>(ds.Employees.OrderBy(el => el.LastName).ThenBy(ef => ef.FirstName));
+        }
+
+        public EmployeeBase EmployeeGetById(int id)
+        {
+            var o = ds.Employees.Find(id);
+
+            return (o == null) ? null : mapper.Map<EmployeeBase>(o);
+        }
+
+        public EmployeeBase EmployeeGetByIdWithDetails(int id)
+        {
+            var o = ds.Employees
+                .Include("Employee1")
+                .Include("Employee2")
+                .SingleOrDefault(e => e.EmployeeId == id);
+
+            return (o == null) ? null : mapper.Map<EmployeeWithDetails>(o);
+        }
+
+
+
+
+
+        // Add some programmatically-generated objects to the data store
+        // Can write one method, or many methods - your decision
+        // The important idea is that you check for existing data first
+        // Call this method from a controller action/method
+
+        public bool LoadData()
+        {
+            // Return if there's existing data
+
+            //if (ds.Your_Entity_Set.Count() > 0) { return false; }
+
+            // Otherwise...
+            // Create and add objects
+            // Save changes
+
+            return true;
+        }
+
+        public bool RemoveData()
+        {
+            try
+            {
+                //foreach (var e in ds.Your_Entity_Set)
+                //{
+                //    ds.Entry(e).State = System.Data.Entity.EntityState.Deleted;
+                //}
+                //ds.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveDatabase()
+        {
+            try
+            {
+                return ds.Database.Delete();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+
+    // New "UserAccount" class for the authenticated user
+    // Includes many convenient members to make it easier to render user account info
+    // Study the properties and methods, and think about how you could use it
+    public class RequestUser
+    {
+        // Constructor, pass in the security principal
+        public RequestUser(ClaimsPrincipal user)
+        {
+            if (HttpContext.Current.Request.IsAuthenticated)
+            {
+                Principal = user;
+
+                // Extract the role claims
+                RoleClaims = user.Claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value);
+
+                // User name
+                Name = user.Identity.Name;
+
+                // Extract the given name(s); if null or empty, then set an initial value
+                string gn = user.Claims.SingleOrDefault(c => c.Type == ClaimTypes.GivenName).Value;
+                if (string.IsNullOrEmpty(gn)) { gn = "(empty given name)"; }
+                GivenName = gn;
+
+                // Extract the surname; if null or empty, then set an initial value
+                string sn = user.Claims.SingleOrDefault(c => c.Type == ClaimTypes.Surname).Value;
+                if (string.IsNullOrEmpty(sn)) { sn = "(empty surname)"; }
+                Surname = sn;
+
+                IsAuthenticated = true;
+                IsAdmin = user.HasClaim(ClaimTypes.Role, "Admin") ? true : false;
+            }
+            else
+            {
+                RoleClaims = new List<string>();
+                Name = "anonymous";
+                GivenName = "Unauthenticated";
+                Surname = "Anonymous";
+                IsAuthenticated = false;
+                IsAdmin = false;
+            }
+
+            NamesFirstLast = $"{GivenName} {Surname}";
+            NamesLastFirst = $"{Surname}, {GivenName}";
+        }
+
+        // Public properties
+        public ClaimsPrincipal Principal { get; private set; }
+        public IEnumerable<string> RoleClaims { get; private set; }
+
+        public string Name { get; set; }
+
+        public string GivenName { get; private set; }
+        public string Surname { get; private set; }
+
+        public string NamesFirstLast { get; private set; }
+        public string NamesLastFirst { get; private set; }
+
+        public bool IsAuthenticated { get; private set; }
+
+        // Add other role-checking properties here as needed
+        public bool IsAdmin { get; private set; }
+
+        public bool HasRoleClaim(string value)
+        {
+            if (!IsAuthenticated) { return false; }
+            return Principal.HasClaim(ClaimTypes.Role, value) ? true : false;
+        }
+
+        public bool HasClaim(string type, string value)
+        {
+            if (!IsAuthenticated) { return false; }
+            return Principal.HasClaim(type, value) ? true : false;
+        }
+    }
+
+}
